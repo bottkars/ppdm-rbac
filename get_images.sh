d996f4757dd490bd19999d6fd9a0922f70aa3b34
bottkars <karsten.bott@bottnet.de>
2024-11-07 09:54:39 +0100

    last

--- get_images.sh
+++ get_images.sh
@@ -1,10 +1,10 @@
 VELERO_PLUGIN=v1.5.3
 BACKUP_DRIVER=v1.5.3
 VELERO=v1.12.1
-DOCKER_SOURCE=docker.io
+DOCKER_SOURCE=harbor.pks.home.labbuildr.com/docker.io
 DOCKER_TARGET=harbor.pks.home.labbuildr.com
 PPDM_RELEASE=19.18.0
-for i in $(seq 08 12);
+for i in $(seq 13 14);
 do
 PPDM_BUILD=${PPDM_RELEASE}-$i
     podman pull ${DOCKER_SOURCE}/dellemc/powerprotect-k8s-controller:${PPDM_BUILD} 

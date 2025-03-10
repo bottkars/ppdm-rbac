db896aab6fc2c46ffb516c8b9d25a9aa9c840cd7
bottkars <karsten.bott@bottnet.de>
2025-03-10 10:29:04 +0100

    sdr updates

--- get_images.sh
+++ get_images.sh
@@ -4,7 +4,7 @@ VELERO=v1.12.1
 DOCKER_SOURCE=harbor.pks.home.labbuildr.com/docker.io
 DOCKER_TARGET=harbor.pks.home.labbuildr.com
 PPDM_RELEASE=19.18.0
-for i in $(seq 13 14);
+for i in {13,14,17,23};
 do
 PPDM_BUILD=${PPDM_RELEASE}-$i
     podman pull ${DOCKER_SOURCE}/dellemc/powerprotect-k8s-controller:${PPDM_BUILD} 
